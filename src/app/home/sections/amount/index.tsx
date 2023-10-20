88ee41d6a82550fd8ad7e534ba347c98c4ac2c00
Mammad Mammadli <memmed199@gmail.com>
2023-10-21 03:54:08 +0400

    device detector

--- src/app/home/sections/amount/index.tsx
+++ src/app/home/sections/amount/index.tsx
@@ -5,7 +5,7 @@ import NumberIncrease from "@/components/NumberIncreaser";
 const Amount = () => {
   return (
     <SectionWrapper>
-      <Container>
+      <Container className="hidden lg:block">
         <div className="lg:px-[64px]">
           <div className="flex flex-col gap-1 lg:gap-4 items-center">
             <NumberIncrease />
