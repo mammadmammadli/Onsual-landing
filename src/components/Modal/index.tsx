8ed8a9345d04cc26f511c7a9ee9f5664ccd8c15b
Mammad Mammadli <memmed199@gmail.com>
2024-02-02 16:45:57 +0400

    first commit

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,6 +1,5 @@
 import { createPortal } from "react-dom";
 import ModalLayer from "./ModalLayer";
-import { AnimatePresence } from "framer-motion";
 
 const Modal = () => {
   return createPortal(<ModalLayer />, document.body);
